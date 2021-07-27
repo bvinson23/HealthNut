89f1af27318161f84398f620bf5add850de2e231
bvinson23 <brandonvinson23@gmail.com>
2021-07-27 14:25:50 -0500

    created form for adding a new note

---
+++ HealthNut/HealthNut/client/src/components/notes/NoteForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { addNote } from "../../modules/noteManager";
+import { Form, FormGroup, Button, Container } from "reactstrap";
+
+const AddNewNote = () => {
+    const [note, setNote] = useState({
+        dateCreated: new Date().toLocaleString(),
+        content: ""
+    });
+
+    const history = useHistory();
+
+    const handleInputChange = (evt) => {
+        const newNote = { ...note }
+        let selectedValue = evt.target.value
+        newNote[evt.target.id] = selectedValue
+        setMeal(newNote)
+    };
+
+    const handleSave = (click) => {
+        click.preventDefault();
+        if (note.content === "") {
+            window.alert("Please fill in all fields")
+        } else {
+            addNote(note)
+                .then(() => history.push("/notes"))
+        };
+    };
+
+    const handleCancelSave = (click) => {
+        click.preventDefault();
+        history.push("/notes")
+    };
+
+    return (
+        <Container className="justified-content-center">
+            <Form>
+                <FormGroup>
+                    <input
+                        type="hidden"
+                        id="dateCreated"
+                        onChange={handleInputChange}
+                        required
+                        className="form-control"
+                        value={note.dateCreated} />
+                </FormGroup>
+                <FormGroup>
+                    <label>Content</label>
+                    <input type="text"
+                        id="content"
+                        onChange={handleInputChange}
+                        required
+                        autoComplete="off"
+                        className="form-control"
+                        placeholder="Write a note..."
+                        value={note.content} />
+                </FormGroup>
+            </Form>
+            <Button className="article-btn"
+                onClick={handleSave}>Save</Button>
+            <Button className="article-btn"
+                variant="warning"
+                onClick={handleCancelSave}>Cancel</Button>
+        </Container>
+    )
+};
+
+export default AddNewNote;
