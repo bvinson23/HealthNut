146317edd33049b0547ab248ff25a19df59da2ee
bvinson23 <brandonvinson23@gmail.com>
2021-07-27 10:48:14 -0500

    created repo for notes, wrote method for getting all notes by user

---
+++ HealthNut/HealthNut/Repositories/NotesRepository.cs
@@ -0,0 +1,57 @@
+﻿using HealthNut.Models;
+using HealthNut.Utils;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthNut.Repositories
+{
+    public class NotesRepository : BaseRepository
+    {
+        public NotesRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Notes> GetAllUserNotes(string firebaseUserId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT n.Id, n.UserId, n.DateCreated, n.Content
+                               u.Name, u.Email
+                        FROM Notes n
+                        JOIN Users u ON u.Id = n.UserId
+                        WHERE u.FirebaseUserId = @FirebaseUserId
+                    ";
+
+                    DbUtils.AddParameter(cmd, "@FirebaseUserId", firebaseUserId);
+                    var reader = cmd.ExecuteReader();
+                    var notes = new List<Notes>();
+                    while (reader.Read())
+                    {
+                        notes.Add(NewNoteFromDb(reader));
+                    }
+
+                    reader.Close();
+
+                    return notes;
+                }
+            }
+        }
+
+        private Notes NewNoteFromDb(SqlDataReader reader)
+        {
+            return new Notes()
+            {
+                Id = DbUtils.GetInt(reader, "Id"),
+                UserId = DbUtils.GetInt(reader, "UserId"),
+                DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
+                Content = DbUtils.GetString(reader, "Content")
+            };
+        }
+    }
+}
