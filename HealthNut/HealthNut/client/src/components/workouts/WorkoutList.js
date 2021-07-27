951f96c649638f6047f751b73818144a98e5e363
bvinson23 <brandonvinson23@gmail.com>
2021-07-27 16:27:40 -0500

    created and setup workoutList

---
+++ HealthNut/HealthNut/client/src/components/workouts/WorkoutList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { NavLink as RRNavLink } from "react-router-dom";
+import {
+    Navbar,
+    NavbarToggler,
+    NavbarBrand
+} from "reactstrap";
+import { getAllWorkouts } from "../../modules/workoutManager";
+import Workout from "./WorkoutCard";
+
+const WorkoutList = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    const [workouts, setWorkouts] = useState([]);
+
+    const getWorkouts = () => {
+        getAllWorkouts().then(workouts => setWorkouts(workouts));
+    };
+
+    useEffect(() => {
+        getWorkouts();
+    }, []);
+
+    return (
+        <div className="container">
+            <Navbar color="light" light expand="md">
+                <NavbarBrand tag={RRNavLink} to="/workouts">Workouts</NavbarBrand>
+                <NavbarToggler onClick={toggle} />
+            </Navbar>
+            {workouts.map((workout) => (
+                <Workout workout={workout} key={workout.id} getWorkouts={getWorkouts} />
+            ))}
+        </div>
+    )
+};
+
+export default WorkoutList;
