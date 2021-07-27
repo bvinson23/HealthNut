f0769b8f47640f188777934c16580441b3059c4c
bvinson23 <brandonvinson23@gmail.com>
2021-07-27 13:09:17 -0500

    wrote get method in controller for getting all notes

--- HealthNut/HealthNut/Controllers/NotesController.cs
+++ HealthNut/HealthNut/Controllers/NotesController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HealthNut.Models;
+using HealthNut.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace HealthNut.Controllers
@@ -11,5 +14,47 @@ namespace HealthNut.Controllers
     [ApiController]
     public class NotesController : ControllerBase
     {
+        private readonly INotesRepository _notesRepository;
+        private readonly IUserRepository _userRepository;
+        public NotesController(INotesRepository notesRepository, IUserRepository userRepository)
+        {
+            _notesRepository = notesRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var user = GetCurrentFirebaseUserId();
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+            else
+            {
+                var notes = _notesRepository.GetAllUserNotes(user);
+                return Ok(notes);
+            }
+        }
+
+        private string GetCurrentFirebaseUserId()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            if (firebaseUserId != null)
+            {
+                return firebaseUserId;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private Users GetCurrentUser()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepository.GetByFirebaseUserId(firebaseUserId);
+        }
     }
 }
