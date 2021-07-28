2434bf8c7faece627958a84c1f9127dccefa7f6b
bvinson23 <brandonvinson23@gmail.com>
2021-07-28 13:32:02 -0500

    created model for current weight

---
+++ HealthNut/HealthNut/Models/Weight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthNut.Models
+{
+    public class Weight
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CurrentWeight { get; set; }
+        public DateTime RecordedDate { get; set; }
+    }
+}
