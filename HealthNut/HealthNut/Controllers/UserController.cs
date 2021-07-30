9dce51270c3c4efdd0b024d29fc55d1d83b983d6
bvinson23 <brandonvinson23@gmail.com>
2021-07-30 15:09:54 -0500

    added authorization to all controllers

--- HealthNut/HealthNut/Controllers/UserController.cs
+++ HealthNut/HealthNut/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using HealthNut.Models;
 using HealthNut.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace HealthNut.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
--- HealthNut/HealthNut/Controllers/WeightController.cs
+++ HealthNut/HealthNut/Controllers/WeightController.cs
@@ -1,15 +1,13 @@
 ﻿using HealthNut.Models;
 using HealthNut.Repositories;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace HealthNut.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class WeightController : ControllerBase
